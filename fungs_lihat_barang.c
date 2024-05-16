5aa4376610447c97c182ba3f658121331bc26216
SifaJ <139639619+slf4A@users.noreply.github.com>
2024-05-16 12:42:19 +0700

    Update fungs_lihat_barang.c

--- fungs_lihat_barang.c
+++ fungs_lihat_barang.c
@@ -2,13 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Nama file tempat menyimpan data barang
 #define FILENAME "tambah_barang.txt"
 
+// Fungsi untuk melihat daftar barang dari file
 void lihat_daftar_barang()
 {
+    // Membuka file dalam mode baca
     FILE *fp = fopen(FILENAME, "r");
     if (fp == NULL)
     {
+        // Jika file gagal dibuka, tampilkan pesan error dan keluar dari fungsi
         printf("Gagal membuka file.\n");
         return;
     }
@@ -17,12 +21,14 @@ void lihat_daftar_barang()
     int jumlah;
     float harga;
 
+    // Mencetak header untuk daftar barang
     printf("Daftar Barang:\n");
+    // Membaca data dari file dan menampilkan informasi barang
     while (fscanf(fp, "%s %d %f", nama, &jumlah, &harga) == 3)
     {
         printf("Nama Barang: %s\n", nama);
         printf("Jumlah Barang: %d\n", jumlah);
         printf("Harga Barang: %.2f\n", harga);
     }
-    fclose(fp);
+    fclose(fp); // Menutup file setelah selesai membaca
 }
